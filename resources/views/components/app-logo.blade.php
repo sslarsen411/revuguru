26d7cd725369c45e02889af57f9a08c3d25bf8bd
Scott Larsen <sslarsen411@gmail.com>
2025-08-24 07:12:04 -0700

    Basic design of site

---
+++ resources/views/components/app-logo.blade.php
@@ -0,0 +1,2 @@
+@props(['size'])
+<img src="{{ asset('https://cdn.mojoimpact.com/twoshakes/shari.webp')}}" class="{{$size}} self-center" alt="Two Shakes Logo">
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+@props(['step' => ''])
+<header class="flex flex-col gap-3 py-4 w-full">
+    <h1 class="self-start text-2xl md:text-3xl lg:text-4xl m-4">
+        Rave Review Guru {{$step}}
+    </h1>
+
+</header>
